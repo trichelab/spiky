47a6ab734e122763aa0d2857930a8a563b625451
Lauren McKinnon <laurenmckinnon77@gmail.com>
2021-01-26 15:06:01 -0500

    Updating methylation_specificity and vignette

--- R/methylation_specificity.R
+++ R/methylation_specificity.R
@@ -4,10 +4,15 @@
 #' (ideally 98-99%) due to the nature of the assay. 
 #'
 #' @export
-methylation_specificity <- function(spike_data, ...) { 
-  data("spike")
-  methylated_spikes <- rownames(spike[spike$methylated == 1,])
-  sum(spike_data[methylated_spikes])
-  methyl_spec <- sum(spike_data[methylated_spikes]) / (sum(spike_data))
-  return(methyl_spec)
+methylation_specificity <- function(ssb_res, ...) { 
+  methreads <- covg_to_df(ssb_res, meth=TRUE)$read_count
+  totalreads <- covg_to_df(ssb_res, meth=FALSE)$read_count
+  meth_spec <- list("mean" = mean(methreads/totalreads), 
+                    "median" = median(methreads/totalreads))
+  return(meth_spec)
+  # data("spike")
+  # methylated_spikes <- rownames(spike[spike$methylated == 1,])
+  # sum(spike_data[methylated_spikes])
+  # methyl_spec <- sum(spike_data[methylated_spikes]) / (sum(spike_data))
+  # return(methyl_spec)
 }
