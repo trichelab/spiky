fb32695db3ec406aaa91640bcfe4ef41c923c818
Lauren McKinnon <laurenmckinnon77@gmail.com>
2021-01-25 20:10:57 -0500

    Adding methylation_specificity and updating vignette

--- R/methylation_specificity.R
+++ R/methylation_specificity.R
@@ -4,8 +4,10 @@
 #' (ideally 98-99%) due to the nature of the assay. 
 #'
 #' @export
-methylation_specificity <- function(x, ...) { 
-
-  stop("unfinished implementation")
-
+methylation_specificity <- function(spike_data, ...) { 
+  data("spike")
+  methylated_spikes <- rownames(spike[spike$methylated == 1,])
+  sum(spike_data[methylated_spikes])
+  methyl_spec <- sum(spike_data[methylated_spikes]) / (sum(spike_data))
+  return(methyl_spec)
 }
