ece4149ad660d071fb5bb17047e2b020218ff424
Tim Triche, Jr <tim.triche@gmail.com>
2021-03-23 17:50:55 -0400

    add cram counts, bayes additive model, spike verification

---
+++ R/model_bam_pmol.R
@@ -0,0 +1,39 @@
+#' Build a Bayesian additive model from spike-ins to correct bias in *-seq
+#'
+#' @param   x     data with assorted feature information (GCfrac, CpGs, etc)
+#' @param   conc  concentration for each spike (must be provided!)
+#' @param   fm    model formula (conc ~ read_count + fraglen + GCfrac + CpGs_3) 
+#' @param   ...   other arguments to pass to `bamlss`
+#'
+#' @return        the model fit for the data
+#' 
+#' @examples 
+#' 
+#' library(bamlss)
+#' data(spike_cram_counts)
+#' scc <- add_frag_info(spike_cram_counts, spike=spike)
+#' scc$conc <- scc$conc * 0.9 # adjust for dilution
+#' scc$CpGs_3 <- scc$CpGs ** 3
+#' fit0 <- model_bam_standards(scc, 
+#'                             fm=conc ~ read_count + fraglen)
+#' fit1 <- model_bam_standards(scc,
+#'                             fm=conc ~ read_count + fraglen + GCfrac + CpGs_3)
+#' DIC(fit0, fit1)
+#' 
+#' @import        bamlss 
+#'
+#' @export
+model_bam_standards <- function(x, conc=NULL, fm=NULL, ...) { 
+
+  if (is.null(fm)) fm <- conc ~ read_count + fraglen + GCfrac + CpGs_3
+  if (is.null(x$conc) & !is.null(conc)) x$conc <- conc
+
+  cols <- strsplit(as.character(fm)[3], " + ", fixed=TRUE)[[1]]
+  if (!all(cols %in% names(x))) {
+    stop("Input data must have columns ", paste(cols, collapse=", "))
+  } 
+
+  # Gaussian model by default -- can alter with ... params 
+  bamlss(fm, data=x, ...)
+
+}
--- R/process_spikes.R
+++ R/process_spikes.R
@@ -1,8 +1,8 @@
 #' QC, QA, and processing for a new spike database 
 #' 
-#' Never trust anyone, least of all yourself 
+#' Sequence feature verification: never trust anyone, least of all yourself.
 #'
-#' @param fasta         fasta file with new spike sequences 
+#' @param fasta         fasta file (or GRanges or DataFrame) w/spike sequences 
 #' @param methylated    whether CpGs in each are methylated (0 or 1, default 0) 
 #' @param ...           additional arguments, e.g. kernels (currently unused)
 #' 
@@ -17,10 +17,18 @@
 #' phages <- system.file("extdata", "phages.fa", package="spiky", mustWork=TRUE)
 #' process_spikes(phages)
 #' 
+#' data(phage)
+#' identical(process_spikes(phage), phage)
+#' identical(phage, process_spikes(phages))
+#'
 #' data(spike)
 #' spikes <- system.file("extdata", "spikes.fa", package="spiky", mustWork=TRUE)
 #' spikemeth <- spike$methylated
 #' process_spikes(spikes, spikemeth)
+#' 
+#' data(genbank_mito) 
+#' process_spikes(genbank_mito) # see also inst/extdata/genbank_mito.R 
+#' gb_mito <- system.file("extdata", "genbank_mito.R", package="spiky")
 #' 
 #' # not run
 #' # library(kebabs)
@@ -35,8 +43,18 @@
 #' @export
 process_spikes <- function(fasta, methylated=0, ...) {
 
-  stopifnot(file.exists(fasta))
-  spikes <- DataFrame(sequence=readDNAStringSet(fasta), methylated=methylated)
+  if (is.character(fasta) && file.exists(fasta)) { 
+    spikes <- DataFrame(sequence=readDNAStringSet(fasta), methylated=methylated)
+  } else if (is(fasta, "GRanges") && "sequence" %in% names(mcols(fasta))) {
+    spikes <- fasta
+    if (!"methylated" %in% names(mcols(fasta))) spikes$methylated <- methylated
+  } else if (is(fasta, "DataFrame") && "sequence" %in% names(fasta)) {
+    spikes <- fasta
+    if (!"methylated" %in% names(fasta)) spikes$methylated <- methylated
+  } else {
+    stop(fasta, " does not exist or is not in a suitable format to proceed.")
+  }
+
   spikes$CpGs <- dinucleotideFrequency(spikes$sequence)[, "CG"]
   GC <- apply(alphabetFrequency(spikes$sequence)[, c("C", "G")], 1, sum)
   spikes$GCfrac <- round(GC / width(spikes$sequence), 2)
